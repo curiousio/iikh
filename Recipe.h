7d9b8ca819ec8ce19322690be12f73c007cbaf0b
curiousio <kyeongmin@glowat.com>
2023-10-10 20:07:39 +0900

    Refactor Recipe.h

--- Recipe.h
+++ Recipe.h
@@ -1,72 +1,75 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 #include <utility>
 #include <vector>
-#include <string>
 
 class Recipe {
  private:
-  std::string menu_name;
-  std::string menu_description;
-  std::string menu_ingredient;
-  std::string menu_recipe;
+  std::string menuName;
+  std::string menuDescription;
+  std::string menuIngredient;
+  std::string menuRecipe;
 
  public:
-  //constructor
+  // constructor
   Recipe() = default;
 
-  //getter
-  std::string getMenuName() { return menu_name; }
-  std::string getMenuDescription() { return menu_description; }
-  std::string getMenuIngredient() { return menu_ingredient; }
-  std::string getMenuRecipe() { return menu_recipe; }
+  // getter
+  std::string getMenuName() { return menuName; }
+  std::string getMenuDescription() { return menuDescription; }
+  std::string getMenuIngredient() { return menuIngredient; }
+  std::string getMenuRecipe() { return menuRecipe; }
 
-  //setter
-  void setMenuName(const std::string &name) { menu_name = name; }
-  void setMenuDescription(const std::string &description) { menu_description = description; }
-  void setMenuIngredient(const std::string &ingredient) { menu_ingredient = ingredient; }
-  void setMenuRecipe(const std::string &recipe) { menu_recipe = recipe; }
+  // setter
+  void setMenuName(const std::string &name) { menuName = name; }
+  void setMenuDescription(const std::string &description) {
+    menuDescription = description;
+  }
+  void setMenuIngredient(const std::string &ingredient) {
+    menuIngredient = ingredient;
+  }
+  void setMenuRecipe(const std::string &recipe) { menuRecipe = recipe; }
 
-  //이부분만 따로 분리시켜 놓은 이유. 문제 2-4 해결.
+  // 이 부분만 따로 분리시켜 놓은 이유. 문제 2-4 해결.
   void addRecipe() {
     std::cout << "Input Recipe Name: ";
-    std::getline(std::cin, menu_name);
+    std::getline(std::cin, menuName);
     std::cout << "Input Recipe Description: ";
-    std::getline(std::cin, menu_description);
+    std::getline(std::cin, menuDescription);
     std::cout << "Input Recipe Ingredient: ";
-    std::getline(std::cin, menu_ingredient);
-    std::cout << "Input Recipe Recipe(Enter : next, .. : End)\n";
-    std::vector<std::string> temps;
-    int idx = 1;
+    std::getline(std::cin, menuIngredient);
+    std::cout << "Input Recipe Method (To finish, just press Enter)"
+              << std::endl;
+    int index = 1;
     while (true) {
-      std::cout << "Recipe Recipe " << idx << ": ";
-      std::string temp;
-      std::getline(std::cin, temp);
-      if (temp == "..") {
+      std::cout << "Step " << index << ": ";
+      std::string userInput;
+      std::getline(std::cin, userInput);
+      if (userInput.compare("") == 0) {
         break;
       }
-      menu_recipe += std::to_string(idx++) + ". ";
-      menu_recipe += temp;
-      menu_recipe += "\n";
+      menuRecipe += std::to_string(index++) + ". ";
+      menuRecipe += userInput;
+      menuRecipe += "\n";
     }
   }
 
   void printNameAndDescription() {
     std::cout << "----------------------------------------" << std::endl;
-    std::cout << "Name: " << menu_name << std::endl;
-    std::cout << "Description: " << menu_description << std::endl;
+    std::cout << "Name: " << menuName << std::endl;
+    std::cout << "Description: " << menuDescription << std::endl;
     std::cout << "----------------------------------------" << std::endl;
   }
 
   void printRecipe() {
     std::cout << "----------------------------------------" << std::endl;
-    std::cout << "Name: " << menu_name << std::endl;
-    std::cout << "Description: " << menu_description << std::endl;
-    std::cout << "Ingredient: " << menu_ingredient << std::endl;
+    std::cout << "Name: " << menuName << std::endl;
+    std::cout << "Description: " << menuDescription << std::endl;
+    std::cout << "Ingredient: " << menuIngredient << std::endl;
     std::cout << "Recipe: " << std::endl;
-    std::cout << menu_recipe << std::endl;
+    std::cout << menuRecipe;  // Already contains newline
     std::cout << "----------------------------------------" << std::endl;
   }
 };
-
